40c3280e0484f94b1be48c325c65f0f39d12e47a
Jørgen Kalsnes Hagen <43886029+Snailedlt@users.noreply.github.com>
2022-07-18 18:15:33 +0200

    Change the suggested file to edit
    Change the suggested file to edit since App.js no longer exists

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <DeviconPlainIcon className="App-logo" size="100px" color="#61dafb" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
